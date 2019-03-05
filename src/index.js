edd61daeaf07859e124537d62fe2b209f5afb1df
唐词伟 <tangciwei@bytedance.com>
2019-03-05 11:33:27 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const spawn = require('child_process').spawn;
+const cwd = process.cwd();
+module.exports = function () {
+    let [type, ...arg] = process.argv.slice(2);
+    if (type === 'pt' || type === 'prettier') {
+        spawn('prettier', ['--write', '--config', '.prettierrc', `${cwd}/*.ts*`]);
+    }
+    else {
+
+    }
+}
