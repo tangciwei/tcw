bf78dcbfa07a40ea2f5d4eeb45140ff4912d80b0
唐词伟 <tangciwei@bytedance.com>
2019-03-06 11:01:21 +0800

    修改ts命令

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const spawn = require('child_process').spawn;
 const cwd = process.cwd();
 module.exports = function () {
     let [type, ...arg] = process.argv.slice(2);
-    if (type === 'pt' || type === 'prettier') {
+    if (type === 'ts' || type === 'prettier') {
         spawn('prettier', ['--write', '--config', '.prettierrc', `${cwd}/*.ts*`]);
     }
     else {
